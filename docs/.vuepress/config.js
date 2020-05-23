0f3fff85473688036e767b730bb52bda6e62d055
Dongmyeong Lee <dongmyeong.lee22@gmail.com>
2020-05-23 12:27:11 +0900

    Default Theme Testing

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,24 @@
 module.exports = {
-    base: "/blog-vuepress/"
+    base: "/blog/",
+    title: "VuePress Blog",
+    themeConfig: {
+        logo: "/img/logo.png",
+        nav: [
+            {text: 'Java', link: '/java/'},
+            {text: 'Spring', link: ''},
+            {
+                text: 'Tags',
+                items: [
+                    {text: 'Database', link: '/items'},
+                    {text: 'JAVA', link: '/items'}
+                ]
+
+            },
+        ]
+    },
+    markdown: {
+        lineNumbers: true,
+    }
 };
+
+// vuepress config 설정 파
---
+++ docs/guide/one.md
@@ -0,0 +1,7 @@
+---
+title: formatter Test
+lang: ko-KR
+---
+
+[Home](/) <!-- Sends the user to the root README.md -->
+[foo heading](./#heading) <!-- Anchors user to a heading in the foo README file -->
