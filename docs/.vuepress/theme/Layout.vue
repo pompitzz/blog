a38358f3a180631d04b38add81f680db4d9be84b
dongmyeonglee <dongmyeong.lee22@gmail.com>
2021-05-08 17:40:53 +0900

    UtterancesComment 추가

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -4,16 +4,21 @@
         v-if="$frontmatter.title"
         slot="page-top"
     />
+    <UtterancesComment
+        v-if="$frontmatter.title"
+        slot="page-bottom"
+    />
   </ParentLayout>
 </template>
 
 <script>
 import ParentLayout from './ParentLayout.vue';
 import PostTitle from '../components/PostTitle.vue';
+import UtterancesComment from '../components/default/UtterancesComment.vue';
 
 export default {
   name: 'Layout',
-  components: { PostTitle, ParentLayout },
+  components: { UtterancesComment, PostTitle, ParentLayout },
 };
 </script>
 
